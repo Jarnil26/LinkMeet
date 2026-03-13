20291991e5025507cdd8860ed68496dce86da574
jarnil <jarnilp@gmail.com>
2026-03-13 13:27:13 +0530

    Allow any origin in CORS and verify production environment URLs

--- src/LinkMeet.API/Program.cs
+++ src/LinkMeet.API/Program.cs
@@ -80,10 +80,9 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+              .AllowAnyMethod();
     });
 });
 
